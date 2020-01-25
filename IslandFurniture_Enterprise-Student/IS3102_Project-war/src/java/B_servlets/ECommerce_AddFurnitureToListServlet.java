6e48f29b23f3fe31016fb08ba8b5c6081e63aea5
Myridian Star <ameenkhan1208@gmail.com>
2020-01-26 01:16:22 +0800

    AddFurnitureServlet edit for tonite

--- IslandFurniture_Enterprise-Student/IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
+++ IslandFurniture_Enterprise-Student/IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+//THEY GOT A SHOPPING CART HELPER CLASS
+import HelperClasses.ShoppingCartLineItem;
+import java.util.ArrayList;
+import javax.servlet.http.HttpSession;
 /**
  *
  * @author Myridian Star
@@ -20,29 +23,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ECommerce_AddFurnitureToListServlet", urlPatterns = {"/ECommerce_AddFurnitureToListServlet"})
 public class ECommerce_AddFurnitureToListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    ArrayList<ShoppingCartLineItem> shoppingCartItems = new ArrayList<>();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ECommerce_AddFurnitureToListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ECommerce_AddFurnitureToListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            System.out.println("ECommerce_AddFurnitureToListServlet processRequest START");
+            HttpSession session = request.getSession();
+            boolean exists;
+            String SKUstring = request.getParameter("SKU");
+            
         }
     }
 
