f956ba98eb71990946f5de09624b398a0396a2a8
Myridian Star <ameenkhan1208@gmail.com>
2020-01-31 19:16:34 +0800

    Progressed on ECommerce_AddFurnitureToListServlet
    i need go solat lah sial

--- IslandFurniture_Enterprise-Student/IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
+++ IslandFurniture_Enterprise-Student/IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
@@ -15,41 +15,107 @@ import javax.servlet.http.HttpServletResponse;
 import HelperClasses.ShoppingCartLineItem;//THEY GOT A SHOPPING CART HELPER CLASS
 import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 /**
  *
  * @author Myridian Star
  */
 @WebServlet(name = "ECommerce_AddFurnitureToListServlet", urlPatterns = {"/ECommerce_AddFurnitureToListServlet"})
 public class ECommerce_AddFurnitureToListServlet extends HttpServlet {
-
+    
     ArrayList<ShoppingCartLineItem> shoppingCartItems = new ArrayList<>();
     private String message;
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            System.out.println("ECommerce_AddFurnitureToListServlet processRequest START");
+        PrintWriter out = response.getWriter();
+        System.out.println("ECommerce_AddFurnitureToListServlet START");
+        try{
             HttpSession session = request.getSession();
-            String SKUstring = request.getParameter("SKU");
-            boolean exists = false;//not initialised smh
-            int id = 0; //shopping cart helperclass has id... //int id has not been initialised
+            // http:// is the URL prefix, which specifies the protocol used to access the location. techterms.com – the server name or IP address of the server. /definition/url – the path to the directory or file.
+            String urlPrefix = (String) session.getAttribute("URLprefix");
             
-            for (int i=0; i<shoppingCartItems.size(); i++) {
-                if(shoppingCartItems.get(i).getSKU().equals(SKUstring)){
-                    exists = true;
-                    id = 1;
-                }
-            }//end of for loop
-            
-            if(exists){
-                shoppingCartItems.get(id).setQuantity(shoppingCartItems.get(id).getQuantity()+1);//add one from existing amount in cart
-                session.setAttribute("shoppingCart", shoppingCartItems);
-                message = "Item added to cart";
-                response.sendRedirect("/IS3102_Project-war/B/SG/shoppingCart.jsp?");
+            if (urlPrefix==null){
+                response.sendRedirect("/IS3102_Project-war/B/SG/index.jsp");
+                return;
             }
             
+            Long countryID = (long) session.getAttribute("countryID");
+            String id = request.getParameter("id");
+            String sku = request.getParameter("SHU");
+            String price = request.getParameter("price");
+            
+            String name = request.getParameter("name");
+            String imageUrl = request.getParameter("imageURL");
+            
+            ShoppingCartLineItem oneItem = new ShoppingCartLineItem();
+            oneItem.setId(id);
+            oneItem.setSKU(sku);
+            oneItem.setName(name);
+            oneItem.setPrice(Double.parseDouble(price));
+            oneItem.setImageURL(imageUrl);
+            oneItem.setCountryID(countryID);
+            oneItem.setQuantity(1);//one item wat
+            int quantity = seeQuantity(countryID, sku);//countryID need be Long
+            
+            ArrayList<ShoppingCartLineItem> shoppingCartItems = (ArrayList<ShoppingCartLineItem>) session.getAttribute("shoppingCart");
+            if(quantity<1){
+                response.sendRedirect("/IS3102_Project-war/B/"+urlPrefix+"shoppingCart.jsp?errMsg=Item unavailable. Cannot add to cart.");
+                return;
+            }
+            if(shoppingCartItems==null){
+                shoppingCartItems = new ArrayList<ShoppingCartLineItem>();
+                shoppingCartItems.add(oneItem);
+            } 
+            else if (!shoppingCartItems.contains(oneItem)){
+                shoppingCartItems.add(oneItem);
+            } 
+            else {
+                if (shoppingCartItems.contains(oneItem)){
+                    for (ShoppingCartLineItem item : shoppingCartItems){
+                        if(item.equals(oneItem)){
+                            if(quantity<item.getQuantity()+1){
+                                response.sendRedirect("/IS3102_Project-war/B/" + urlPrefix + "shoppingCart.jsp?errMsg=Item unavailable. Cannot add to cart.");
+                                return; //64.
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+    
+    public int seeQuantity(Long countryID, String sku){
+        //debug
+        System.out.println("seeQuantity START");
+        System.out.println("SKU = "+sku);
+        try{
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client
+                    .target("http://dmit.bulletplus.com/WebService_Mobile/webresources/entity.countryentity")
+                    .path("getQuantity")
+                    .queryParam("countryID", countryID)
+                    .queryParam("SKU", sku);
+            //from sep practical
+            Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            Response response  = invocationBuilder.get();
+            if(response.getStatus()!=200){
+                return 0;
+            }
+            String incomingResponse = (String) response.readEntity(String.class);
+            return Integer.parseInt(incomingResponse);
+        } catch(Exception ex){
+            System.out.println("Exception Occured!");
+            System.out.println(ex);
+            return 0;// no QUANtity
+        }
+        //return 0;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
